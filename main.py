0fe68308bc43bed2555eec98ede10a54e2e06d71
denischagin <denischaginnn@gmail.com>
2022-06-08 23:42:23 +0300

    commit

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ def dir():
     while True:
         way = input('Введите директорию:')
         if os.path.isdir(way):
+            print('start')
             return way
         else:
             print('Вы ввели неправильный путь к файлу')
 
 
 def dict_way_size(way):
-    print('...Загрузка...')
     dict1 = {}
     for root, dirs, files in os.walk(way):
         for file in files:
             newkey = {os.path.join(root, file): os.path.getsize(root + '\\' + file)}
             dict1.update(newkey)
+    print('Получение файлов завершено, найдено ', len(dict1))
     return dict1
 
 
 def analysis(dict1):
+    print('Поиск дубликатов')
     duplicate = {}
     for key1 in dict1:
         for key2 in dict1:
@@ -36,14 +38,13 @@ def duplicate(duplicate):
     list_print = []
     for key1 in duplicate:
         if key1 not in list_print:
-            print()
             print(f'({duplicate[key1]}, {key1})')
             for key2 in duplicate:
                 if key1[key1.rfind('\\'):] == key2[key2.rfind('\\'):] and duplicate[key1] == duplicate[key2] and not \
                         key1 == key2:
                     print(key2)
                     list_print.append(key2)
-
+            print()
 
 if __name__ == '__main__':
     duplicate(analysis(dict_way_size(dir())))
